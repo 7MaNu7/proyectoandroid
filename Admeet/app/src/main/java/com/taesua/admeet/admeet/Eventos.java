ce1a2959f397bd71c11a757bb1ee5101de88ed81
EncarnaAmoros <encaramorosb@gmail.com>
2015-04-01 22:36:01 +0200

    Cambios de clase

--- Admeet/app/src/main/java/com/taesua/admeet/admeet/Eventos.java
+++ Admeet/app/src/main/java/com/taesua/admeet/admeet/Eventos.java
@@ -1,38 +1,24 @@
 package com.taesua.admeet.admeet;
 
-import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
 
 
-public class MainActivity extends ActionBarActivity {
+public class Eventos extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        //Instanciar elemento
-        Button b = (Button) findViewById(R.id.buttonLogin);
-        //Accion del boton
-        b.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, Eventos.class);
-                startActivity(intent);
-            }
-        });
+        setContentView(R.layout.activity_eventos);
     }
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.menu_eventos, menu);
         return true;
     }
 
@@ -50,5 +36,4 @@ public class MainActivity extends ActionBarActivity {
 
         return super.onOptionsItemSelected(item);
     }
-
 }
