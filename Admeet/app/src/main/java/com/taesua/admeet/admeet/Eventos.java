32c8c672ad7fa4f8a90df4e8f53750413b2c1525
EncarnaAmoros <encaramorosb@gmail.com>
2015-04-04 23:44:48 +0200

    Ver eventos (sin probar)

--- Admeet/app/src/main/java/com/taesua/admeet/admeet/Eventos.java
+++ Admeet/app/src/main/java/com/taesua/admeet/admeet/Eventos.java
@@ -4,14 +4,41 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import com.appspot.ad_meet.conference.Conference;
+
+import java.io.IOException;
+import java.util.ArrayList;
 
 
 public class Eventos extends ActionBarActivity {
+
+    private Conference conferencia;
+    private ListView eventos;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_eventos);
+
+        try {
+           ArrayList a =  new ArrayList();
+           eventos = (ListView)findViewById(R.id.listview);
+           String eve[] = new String[conferencia.getConferencesCreated().size()];
+           a.add( conferencia.getConferencesCreated().getAlt());
+           for(int i=0;i<conferencia.getConferencesCreated().size();i++)
+               eve[i]=conferencia.getConferencesCreated().getAlt();
+           ArrayAdapter<String> adaptador = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, eve);
+            eventos.setAdapter(adaptador);
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
     }
 
 
