ddf5517f33db2c3e5850b863132587aa11cfabdd
EncarnaAmoros <encaramorosb@gmail.com>
2015-04-04 12:15:38 +0200

    Librerías necesarias añadidas Archivos de la api añadidos Recomiendo descargar este emulador: https://geekytheory.com/genymotion-un-rapido-y-eficiente-emulador-android/#windows Es muchísimo más rápido que el que viene con AndroidStudio

--- Admeet/app/src/main/java/com/taesua/admeet/admeet/MainActivity.java
+++ Admeet/app/src/main/java/com/taesua/admeet/admeet/MainActivity.java
@@ -1,8 +1,8 @@
 package com.taesua.admeet.admeet;
 
 import android.content.Intent;
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -15,6 +15,7 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
 
         //Instanciar elemento
         Button b = (Button) findViewById(R.id.buttonLogin);
