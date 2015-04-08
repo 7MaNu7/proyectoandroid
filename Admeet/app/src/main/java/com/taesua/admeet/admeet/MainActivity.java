3f19d5b68ad66aa5bcb35eb88d1977397d78c17b
EncarnaAmoros <encaramorosb@gmail.com>
2015-04-08 19:19:51 +0200

    Intento de login autentificado con google plus, sale pero da un error al conectar Aún así deja pasar a las siguientes activities

--- Admeet/app/src/main/java/com/taesua/admeet/admeet/MainActivity.java
+++ Admeet/app/src/main/java/com/taesua/admeet/admeet/MainActivity.java
@@ -1,31 +1,48 @@
 package com.taesua.admeet.admeet;
 
+import android.app.Activity;
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.content.IntentSender;
 import android.os.Bundle;
-import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
-public class MainActivity extends ActionBarActivity {
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesClient;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.plus.PlusClient;
 
-    private static final String LOG_TAG = "MainActivity";
+public class MainActivity extends /*ActionBar*/Activity implements View.OnClickListener,
+        GooglePlayServicesClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
 
-    /**
-     * Activity result indicating a return from the Google account selection intent.
-     */
-    private static final int ACTIVITY_RESULT_FROM_ACCOUNT_SELECTION = 2222;
+    private static final int REQUEST_CODE_RESOLVE_ERR = 9000;
 
-  //  private AuthorizationCheckTask mAuthTask;
-    private String mEmailAccount;
+    private ProgressDialog mConnectionProgressDialog;
+    private PlusClient mPlusClient;
+    private ConnectionResult mConnectionResult;
 
-   // private ConferenceListFragment mConferenceListFragment;
+    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        //mPlusClient = new PlusClient.Builder(this, this, this).setVisibleActivities("http://schemas.google.com/AddActivity", "http://schemas.google.com/BuyActivity").build();
+        mPlusClient = new PlusClient.Builder(this, this, this).setActions(
+                "http://schemas.google.com/AddActivity", "http://schemas.google.com/BuyActivity")
+                .setScopes("PLUS_LOGIN") // Space separated list of scopes
+                .build();
+
+        // Se tiene que mostrar esta barra de progreso si no se resuelve el fallo de conexión.
+        mConnectionProgressDialog = new ProgressDialog(this);
+        mConnectionProgressDialog.setMessage("Signing in...");
 
         //Instanciar elemento
         Button b = (Button) findViewById(R.id.buttonLogin);
@@ -34,10 +51,105 @@ public class MainActivity extends ActionBarActivity {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(MainActivity.this, Eventos.class);
+
+                try {
+                    String accountName = mPlusClient.getAccountName();
+                    System.out.println("Este es; " + accountName + " el email");
+                }
+                catch(Exception e)
+                {
+                    System.out.println("ERROR mail: " + e.getMessage());
+                }//mi try
+
                 startActivity(intent);
             }
         });
     }
+
+
+
+    @Override
+    protected void onStart() {
+        System.out.println("---ON START!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+        super.onStart();
+        try {
+            mPlusClient.connect();
+        }
+        catch(Exception e)
+        {
+            System.out.println("ERROR conect: " + e.getMessage());
+        }
+    }
+
+    @Override
+    protected void onStop() {
+        System.out.println("---ON STOP!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+        super.onStop();
+        mPlusClient.disconnect();
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult result) {
+        System.out.println("---ON CONNECTIONFAILED!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+        if (result.hasResolution()) {
+            try {
+                result.startResolutionForResult(this, REQUEST_CODE_RESOLVE_ERR);
+            } catch (IntentSender.SendIntentException e) {
+                try {
+                    mPlusClient.connect();
+                }
+                catch(Exception e1)
+                {
+                    System.out.println("ERROR conect: " + e1.getMessage());
+                }//mi try
+            }
+        }
+        // Guarda el resultado y resuelve el fallo de conexión con el clic de un usuario.
+        mConnectionResult = result;
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int responseCode, Intent intent) {
+        System.out.println("---ON ACTIVITY RESULT!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+        if (requestCode == REQUEST_CODE_RESOLVE_ERR && responseCode == RESULT_OK) {
+            mConnectionResult = null;
+            try {
+                mPlusClient.connect();
+            }
+            catch(Exception e)
+            {
+                System.out.println("ERROR conect: " + e.getMessage());
+            }//mi try
+        }
+    }
+
+    @Override
+    public void onConnected(Bundle bundle) {
+        System.out.println("---ON CONNECTED!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+        String accountName = mPlusClient.getAccountName();
+        Toast.makeText(this, accountName + " is connected.", Toast.LENGTH_LONG).show();
+    }
+
+    //@Override
+    public void onConnected() {
+        System.out.println("---ON CONNECTED 2 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+        String accountName = mPlusClient.getAccountName();
+        Toast.makeText(this, accountName + " is connected.", Toast.LENGTH_LONG).show();
+    }
+
+    /*   @Override
+       public void onConnected(Bundle bundle) {
+
+       }
+   */
+    @Override
+    public void onDisconnected() {
+        Log.d(TAG, "disconnected");
+    }
+
+
+
+
 
 
     @Override
@@ -62,4 +174,8 @@ public class MainActivity extends ActionBarActivity {
         return super.onOptionsItemSelected(item);
     }
 
+    @Override
+    public void onClick(View v) {
+
+    }
 }
